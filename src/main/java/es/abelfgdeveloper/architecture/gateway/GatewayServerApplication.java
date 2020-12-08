074cbfd807fefb9c2b2a520394c95f2fb9c5da1b
Abel <abeldevelop@users.noreply.github.com>
2020-12-08 01:16:15 +0100

    Create the structure

---
+++ src/main/java/es/abelfgdeveloper/architecture/gateway/GatewayServerApplication.java
@@ -0,0 +1,14 @@
+package es.abelfgdeveloper.architecture.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayServerApplication.class, args);
+  }
+}
---
+++ src/test/java/es/abelfgdeveloper/architecture/gateway/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.abelfgdeveloper.architecture.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
